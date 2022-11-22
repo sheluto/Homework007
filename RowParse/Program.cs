811f6f737aac0f219b9a9107ed83a6dc8863bb8f
«Sheluto» <sheluto@gmail.com>
2022-11-23 00:05:38 +0300

    strange

---
+++ RowParse/Program.cs
@@ -0,0 +1,23 @@
+﻿// Парсинг строки. В строке находятся парные координаты точек фигуры
+//Нужно увеличить каждую из координат ровно в 2 раза и показать пользователю ответ
+
+
+using System.Linq;
+
+
+string text = "(1,2) (2,3) (4,5) (6,7)";
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+var data = text.Split(" ");
+                .Select(item => item.Split(','))
+                .Select(e) => (x: int.Parse(e[0]),y: int.Parse(e[1]))
+                .Where (e => e.x%2 ==0)
+                .Select(point => (point.x*10, point.y+10)
+                .ToArray();
+
+
+for (int i=0; i<data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
